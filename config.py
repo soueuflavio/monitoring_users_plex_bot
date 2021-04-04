c247045935a546861651df5c960ec332b72f9951
FLAVIO PEREIRA <flavio.alves.pereira@gmail.com>
2021-04-04 18:42:20 +0000

    Primeiro Commit

---
+++ config.py
@@ -0,0 +1,17 @@
+"""
+configuration file
+"""
+
+# Variable to configure
+TOKEN_ID = 'Your token_id'
+CHAT_ID = 'Your chat_id' 
+
+# Variable that can be modified
+VIDEO_TIME = 60  # duration of video recording in second
+
+# Variable para Plexpy
+tautulli_apikey = 'a44ab52097d2471b84605e381efe34f3'
+tautulli_url_port = '192.168.0.21:8181'
+
+# Variable para Docker Daemon, for access Docker`s API
+docker_nas_url = '192.168.0.21:5555'
